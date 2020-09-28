673992cbf9ebedf92410496a438e807102c5e42e
Kalin Katev <kalin.r.katev@gmail.com>
2020-09-28 14:59:20 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload. Or don't, I don't care. Srsly.
         </p>
         <a
           className="App-link"
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React NOW!
         </a>
       </header>
     </div>
